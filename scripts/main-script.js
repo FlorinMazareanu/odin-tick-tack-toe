cab8df0c913f136155ca5b01f6550ecb4579e5b8
FlorinMazareanu <florin.mazareanu@gmail.com>
2023-02-22 22:34:25 +0200

    1.2

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,1 +1,25 @@
-console.log("test");
+//multidimensional array to represent the squares and their content
+let squareMatrix = [];
+
+//factory function to fill the squares
+//n will eventually become either x or 0
+//i and j are just positions like in a C++ matrix
+const squareFactory = (xo, i, j) => {
+    return {xo, i, j};
+};
+
+//filling up squareMatrix with initial values
+for (let i=0; i<=2; i++) {
+    let arr = [];
+    for(let j=0; j<=2; j++) {
+        const newSquare = squareFactory("n", i, j);
+        arr.push(newSquare);
+    }
+    squareMatrix.push(arr);
+}
+
+console.log(squareMatrix);
+console.log(squareMatrix[0][0].xo);
+
+
+
